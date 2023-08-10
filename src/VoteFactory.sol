c064db02b6fec0f8aa44daf9c075405dbd5d74d3
0xpluto <jackson2020@me.com>
2023-08-10 10:18:05 -0500

    seperate contract for 2 yes options

--- src/VoteFactory.sol
+++ src/VoteFactory.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "./VoteTracker.sol";
+import "./VoteTracker2.sol";
 import "solmate/auth/Owned.sol";
 
 contract VoteFactory is Owned {
@@ -12,10 +13,15 @@ contract VoteFactory is Owned {
 
     constructor() Owned(msg.sender) {}
 
-    function startVote(uint32 length, uint64 fipNum) public onlyOwner returns (address vote) {
+    function startVote(uint32 length, uint64 fipNum, bool doubleYesOption) public onlyOwner returns (address vote) {
         require(FIPnumToAddress[fipNum] == address(0), "Vote already exists for this FIP");
 
-        bytes memory bytecode = type(VoteTracker).creationCode;
+        bytes memory bytecode;
+        if (doubleYesOption) {
+            bytecode = type(VoteTrackerDoubleYes).creationCode;
+        } else {
+            bytecode = type(VoteTracker).creationCode;
+        }
         bytes32 salt = keccak256(abi.encodePacked(fipNum, length));
         assembly {
             vote := create2(0, add(bytecode, 32), mload(bytecode), salt)
