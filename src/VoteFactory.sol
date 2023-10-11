e5ae91ef78746fbf13a9f840decc64d009231d54
Ayden Brewer <dalegribble@riseup.net>
2023-10-10 22:16:20 -0700

    Move registration from VoteTracker to VoteFactory

--- src/VoteFactory.sol
+++ src/VoteFactory.sol
@@ -28,6 +28,10 @@ ____    ____  ______   .___________. __  .__   __.   _______
 
 */
 
+error AlreadyRegistered();
+error NotOwner();
+error MinerAlreadyRegistered();
+
 contract VoteFactory is Owned {
 
 
@@ -38,14 +42,21 @@ contract VoteFactory is Owned {
 
     address[] public deployedVotes;
     mapping(address => bool) public starters;
+    mapping(address => uint64[]) public ownedMiners;
+    mapping(uint64 => address) public registeredMiner;
+    mapping(address => address) public ownedGlifPool;
+    mapping(address => bool) public registered;
 
     mapping (uint32 => address) public FIPnumToAddress;
+
+    address constant glifFactory = address(0x526Ab27Af261d28c2aC1fD24f63CcB3bd44D50e0);
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                           Events                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     event VoteStarted(address vote, uint32 fipNum, uint32 length);
+    event VoterRegistered(address voter, address glif, uint64[] minerIds);
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                          Modifers                          */
@@ -90,6 +101,117 @@ contract VoteFactory is Owned {
         deployedVotes.push(vote);
         
         emit VoteStarted(vote, fipNum, length);
+    }
+
+    function register(address glifpool, uint64[] calldata minerIds) public {
+        // Do not let user register twice
+        if (
+            registered[msg.sender]
+        ) {
+            revert AlreadyRegistered();
+        }
+
+        // Determine if glifpool is valid
+        bool glif = (GlifFactory(glifFactory).isAgent(glifpool) &&
+            Owned(glifpool).owner() == msg.sender);
+
+        // Collect RBP voting weight
+        uint length = minerIds.length;
+        for (uint i = 0; i < length; ++i) {
+            uint64 minerId = minerIds[i];
+
+            if (registeredMiner[minerId]) revert MinerAlreadyRegistered();
+
+            // Add their RBP voting weight
+            address minerOwner = glif ? glifpool : msg.sender;
+
+            registeredMiner[minerId] = msg.sender;
+        }
+
+        if (glif) {
+            ownedGlifPool[msg.sender] = glifpool;
+        }
+
+        registered[msg.sender] = true;
+
+        // Finalize state changes
+        emit VoterRegistered(msg.sender, glifpool, minerIds);
+    }
+
+    function addMiner(address voter, uint64 minerId) public {
+        if (!isMiner(minerId, msg.sender)) {
+            revert NotOwner();
+        }
+
+        if (!registered[voter]) {
+            revert NotRegistered();
+        }
+
+        registeredMiner[minerId] = voter;
+        ownedMiners[voter] = ownedMiners[voter].push(minerId);
+    }
+
+    function isMiner(
+        uint64 minerId,
+        address sender
+    ) internal view returns (bool) {
+        if (minerId == 0) {
+            return false;
+        }
+        return
+            MinerAPI.isControllingAddress(
+                CommonTypes.FilActorId.wrap(minerId),
+                toFilAddr(sender)
+            );
+    }
+
+    function toFilAddr(
+        address addr
+    ) public pure returns (CommonTypes.FilAddress memory filAddr) {
+        bytes memory delegatedAddr = abi.encodePacked(hex"040a", addr);
+        filAddr = CommonTypes.FilAddress(delegatedAddr);
+    }
+
+    /// @notice Calculates the voting power of a voter for a single miner
+    /// @notice If voting power is zero, voting power is calculated off of FIL balance and LSD token balances
+    /// @param minerId The miner to calculate voting power for
+    /// @param voter The address of the voter
+    /// @return power The voting power of the voter
+    function voterRBP(
+        uint64 minerId,
+        address voter
+    ) public view returns (uint256 power) {
+        bool isminer = isMiner(minerId, voter);
+        if (!isminer) return 0;
+
+        // Vote weight as a miner
+        PowerTypes.MinerRawPowerReturn memory pow = PowerAPI.minerRawPower(
+            uint64(minerId)
+        );
+        CommonTypes.BigInt memory p = pow.raw_byte_power;
+
+        if (p.neg) {
+            return 0;
+        }
+
+        bytes memory rpower = p.val;
+        assembly {
+            // Length of the byte array
+            let length := mload(rpower)
+
+            // Load the bytes from the memory slot after the length
+            // Assuming power is > 32 bytes is okay because 1 PiB
+            // is only 1e16
+            let _bytes := mload(add(rpower, 0x20))
+            let shift := mul(sub(0x40, mul(length, 2)), 0x04)
+
+            // bytes slot will be left aligned
+            power := shr(shift, _bytes)
+        }
+    }
+
+    function getOwnedMinerLength(address owner) external returns (uint256 length) {
+        return ownedMiners[owner].length;
     }
 
     function addStarter(address starter) public onlyOwner {
--- src/VoteTracker.sol
+++ src/VoteTracker.sol
@@ -11,6 +11,8 @@ import "filecoin-solidity/types/PowerTypes.sol";
 
 import "solmate/auth/Owned.sol";
 
+import "./interfaces/VoteFactory.sol";
+
 contract VoteTracker is Owned {
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -25,17 +27,14 @@ contract VoteTracker is Owned {
     address[] public lsdTokens;
     string public question;
 
-    mapping(address => uint256) public voterWeightRBP;
-    mapping(address => uint256) public voterWeightMinerToken;
-    mapping(address => uint256) public voterWeightToken;
     mapping(address => bool) public hasVoted;
-    mapping(uint64 => bool) public registeredMiner;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                      Internal Storage                      */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     address constant glifFactory = address(0x526Ab27Af261d28c2aC1fD24f63CcB3bd44D50e0);
+    IVoteFactory immutable factory;
 
     // Note: Tallies are initialized at 1 to save gas and keep warm storage
 
@@ -67,24 +66,15 @@ contract VoteTracker is Owned {
         uint256 weightToken,
         uint256 vote
     );
-    event VoterRegistered(
-        address voter,
-        uint64[] minerIds,
-        uint256 weightRBP,
-        uint256 weightToken
-    );
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                           Errors                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     error AlreadyVoted();
-    error NotRegistered();
-    error AlreadyRegistered();
     error VoteNotConcluded();
     error VoteConcluded();
     error InvalidGlifPool();
-    error MinerAlreadyRegistered();
     error InvalidMiner();
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -118,7 +108,7 @@ contract VoteTracker is Owned {
     /// @notice Checks if the sender is a registered voter
     /// @param sender The address to check
     modifier isRegistered(address sender) {
-        if (voterWeightRBP[sender] == 0 && voterWeightToken[sender] == 0 && voterWeightMinerToken[sender] == 0) {
+        if (!factory.registered(sender)) {
             revert NotRegistered();
         }
         _;
@@ -133,6 +123,7 @@ contract VoteTracker is Owned {
     /// @param _lsdTokens The addresses of the LSD tokens to count as voting power
     /// @param owner The owner of the vote
     constructor(
+        address factoryAddress,
         uint32 length,
         string[2] memory _yesOptions,
         address[] memory _lsdTokens,
@@ -140,6 +131,7 @@ contract VoteTracker is Owned {
         address owner,
         string memory _question
     ) Owned(owner) {
+        factory = IVoteFactory(factoryAddress);
         yesOptions = _yesOptions;
         FIP = _FIP;
         voteLength = length;
@@ -152,32 +144,13 @@ contract VoteTracker is Owned {
     /*                      Public Functions                      */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @notice A combination function of `castVote` and `registerVoter`
-    /// @notice If not registering for a glif pool, pass in address(0)
-    /// @notice If don't have any minerId's pass in an empty list
-    /// @param vote The vote to cast
-    /// @param glifPool The address of the glifpool to register for, address(0) if not using glif pools
-    /// @param minerIds The miner IDs to register for
-    /// @return voteWeightRBP The voting power of the voter in Raw Byte Power
-    /// @return voteWeightToken The voting power of the voter in FIL and LSD's
-    function voteAndRegister(
-        uint256 vote,
-        address glifPool,
-        uint64[] calldata minerIds
-    ) public returns (uint256 voteWeightRBP, uint256 voteWeightToken) {
-        (voteWeightRBP, voteWeightToken) = registerVoter(glifPool, minerIds);
-        castVote(vote);
-    }
-
     /// @notice Msg sender must be a registered voter
     /// @param vote The vote to cast
     function castVote(
         uint256 vote
     ) public voting(msg.sender) isRegistered(msg.sender) {
         uint vote_num = vote % 3;
-        uint weightRBP = voterWeightRBP[msg.sender];
-        uint weightMinerToken = voterWeightMinerToken[msg.sender];
-        uint weightToken = voterWeightToken[msg.sender];
+        (uint weightToken, uint weightRBP, uint weightMinerToken) = getVotingPower(msg.sender);
 
         // YES VOTE
         if (vote_num == 0) {
@@ -203,82 +176,6 @@ contract VoteTracker is Owned {
         }
 
         emit VoteCast(msg.sender, weightRBP, weightToken, vote % 6);
-    }
-
-    /// @notice Msg sender must be a controlling address for the miner
-    /// @notice If not registering for a miner, pass in address(0)
-    /// @param minerIds The miner IDs to register for
-    /// @param glifpool The address of the glifpool to register for, address(0) if not using glif pools
-    /// @return powerRBP The voting power in Raw Byte Power of the voter
-    /// @return powerToken The voting power in FIL and LSD's of the voter
-    function registerVoter(
-        address glifpool,
-        uint64[] calldata minerIds
-    ) public returns (uint256 powerRBP, uint256 powerToken) {
-        // Do not let user register twice
-        if (
-            voterWeightRBP[msg.sender] > 0 || voterWeightToken[msg.sender] > 0
-        ) {
-            revert AlreadyRegistered();
-        }
-
-        if (uint32(block.timestamp) > voteStart + voteLength) {
-            revert VoteConcluded();
-        }
-
-        // Determine if glifpool is valid
-        bool glif = (GlifFactory(glifFactory).isAgent(glifpool) &&
-            Owned(glifpool).owner() == msg.sender);
-        
-        if (glifpool != address(0) && !glif) {
-            revert InvalidGlifPool();
-        }
-
-        // Collect RBP voting weight
-        uint length = minerIds.length;
-        for (uint i = 0; i < minerIds.length; ++i) {
-            uint64 minerId = minerIds[i];
-
-            if (registeredMiner[minerId]) revert MinerAlreadyRegistered();
-
-            // Add their RBP voting weight
-            address minerOwner = glif ? glifpool : msg.sender;
-
-            // Set the RBP voting weight
-            uint rbp = voterRBP(minerId, minerOwner);
-            if (rbp == 0) continue;
-
-            powerRBP += rbp;
-
-            registeredMiner[minerId] = true;
-        }
-
-        // Collect FIL voting weight
-        powerToken += msg.sender.balance;
-
-        // Collect LSD voting weight
-        length = lsdTokens.length;
-        for (uint i = 0; i < length; ++i) {
-            ERC20 token = ERC20(lsdTokens[i]);
-
-            uint balance = token.balanceOf(msg.sender);
-
-            powerToken += balance;
-        }
-
-        // Finalize state changes
-        emit VoterRegistered(msg.sender, minerIds, powerRBP, powerToken);
-
-        voterWeightRBP[msg.sender] = powerRBP;
-
-        // If they have RBP then assign their token power to the miner's
-        if (powerRBP > 0) {
-            voterWeightMinerToken[msg.sender] = powerToken;
-
-            // If they have no RBP then assign to normal token category
-        } else {
-            voterWeightToken[msg.sender] = powerToken;
-        }
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -410,71 +307,6 @@ contract VoteTracker is Owned {
     /*                     Internal Functions                     */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @notice Checks if an address is a controlling address for a miner
-    /// @param minerId The miner to check
-    /// @param sender The address to check
-    /// @return isMiner True if the address is a controlling address for the miner
-    function isMiner(
-        uint64 minerId,
-        address sender
-    ) internal view returns (bool) {
-        if (minerId == 0) {
-            return false;
-        }
-        return
-            MinerAPI.isControllingAddress(
-                CommonTypes.FilActorId.wrap(minerId),
-                toFilAddr(sender)
-            );
-    }
-
-    /// @notice Calculates the voting power of a voter for a single miner
-    /// @notice If voting power is zero, voting power is calculated off of FIL balance and LSD token balances
-    /// @param minerId The miner to calculate voting power for
-    /// @param voter The address of the voter
-    /// @return power The voting power of the voter
-    function voterRBP(
-        uint64 minerId,
-        address voter
-    ) public view returns (uint256 power) {
-        bool isminer = isMiner(minerId, voter);
-        if (!isminer) return 0;
-
-        // Vote weight as a miner
-        PowerTypes.MinerRawPowerReturn memory pow = PowerAPI.minerRawPower(
-            uint64(minerId)
-        );
-        CommonTypes.BigInt memory p = pow.raw_byte_power;
-
-        if (p.neg) {
-            return 0;
-        }
-
-        bytes memory rpower = p.val;
-        assembly {
-            // Length of the byte array
-            let length := mload(rpower)
-
-            // Load the bytes from the memory slot after the length
-            // Assuming power is > 32 bytes is okay because 1 PiB
-            // is only 1e16
-            let _bytes := mload(add(rpower, 0x20))
-            let shift := mul(sub(0x40, mul(length, 2)), 0x04)
-
-            // bytes slot will be left aligned
-            power := shr(shift, _bytes)
-        }
-    }
-
-    /// @notice Converts an address to a filecoin address
-    /// @param addr The address to convert
-    /// @return filAddr The filecoin address
-    function toFilAddr(
-        address addr
-    ) internal pure returns (CommonTypes.FilAddress memory filAddr) {
-        bytes memory delegatedAddr = abi.encodePacked(hex"040a", addr);
-        filAddr = CommonTypes.FilAddress(delegatedAddr);
-    }
 
     /// @notice If this vote has two yes options then this function will put it in the correct category
     /// @notice This function should only be called if the vote param modulo 3 == 0
@@ -498,10 +330,11 @@ contract VoteTracker is Owned {
     }
 
     function hasRegistered(address voter) public view returns (bool) {
-        return voterWeightRBP[voter] != 0 || voterWeightToken[voter] != 0 || voterWeightMinerToken[voter] != 0;
+        return factory.registered(voter);
     }
 
-    function getVotingPower(address voter, address glifpool, uint64[] calldata minerIds) public view returns (uint256 tokenPower, uint256 bytePower) {
+    function getVotingPower(address voter) public view returns (uint256 tokenPower, uint256 bytePower, uint256 minerTokenPower) {
+        address glifpool = factory.ownedGlifPool(voter);
         // Determine if glifpool is valid
         bool glif = (GlifFactory(glifFactory).isAgent(glifpool) &&
             Owned(glifpool).owner() == voter);
@@ -511,15 +344,16 @@ contract VoteTracker is Owned {
         }
         
         // Collect RBP voting weight
+        uint64[] storage minerIds = factory.ownedMiners(voter);
         uint length = minerIds.length;
-        for (uint i = 0; i < minerIds.length; ++i) {
+        for (uint i = 0; i < length; ++i) {
             uint64 minerId = minerIds[i];
 
             // Add their RBP voting weight
             address minerOwner = glif ? glifpool : voter;
 
             // Set the RBP voting weight
-            uint rbp = voterRBP(minerId, minerOwner);
+            uint rbp = factory.voterRBP(minerId, minerOwner);
             if (rbp == 0) continue;
 
             bytePower += rbp;
@@ -538,7 +372,14 @@ contract VoteTracker is Owned {
             tokenPower += balance;
         }
 
-        return (tokenPower, bytePower);
+        // If they have RBP then assign their token power to the miner's
+        if (bytePower > 0) {
+            return (0, bytePower, tokenPower);
+
+            // If they have no RBP then assign to normal token category
+        } else {
+            return (tokenPower, bytePower, 0);
+        }
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
